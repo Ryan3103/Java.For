b06e5bd93c35a5ab9f7c63fd6e154528b588875d
Ryan Gabriel <ryangabrielsilvag052@gmail.com>
2025-02-04 16:49:11 -0300

    Add files via upload

---
+++ For/Ativ_24.java
@@ -0,0 +1,10 @@
+package Java.For;
+
+public class Ativ_24 {
+        public static void main(String[] args) {
+            for (int n = 1; n <= 10; n++) {
+                int numeroTriangular = (n * (n + 1)) / 2;
+                System.out.println(numeroTriangular);
+            }
+        }
+    }
